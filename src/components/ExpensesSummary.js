ab75b06f22bf28bc8d151b7d06cc60236c1dc84e
BYRNE-PC\Warren Byrne <warren.byrne@rezatec.com>
2019-06-28 14:35:06 +0100

    updates to expense summary component

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import numeral from 'numeral';
 
 import selectExpenses from '../selectors/expenses';
 import getExpensesTotal from '../selectors/expenses-total';
 
-const ExpensesSummary = (props) => (
-  <div>
-    <p>
-      Viewing {props.expenses.length}
-      {props.expenses.length === 1 ? ' expense ' : ' expenses '}
-      totalling {numeral(props.total / 100).format('$0,0.[00]')}
+export const ExpensesSummary = ({ expenses, total }) => {
+  const word = expenses === 1 ? 'expense' : 'expenses'
+  const totalAmount = numeral(total / 100).format('$0,0.[00]')
+  return (
+    <div>
+      <p>
+        Viewing {expenses.length} {word} totalling {totalAmount}
       </p>
-  </div>
-);
+    </div>
+  )
+}
 
 const mapStateToProps = (state) => {
+  const visibleExpenses = selectExpenses(state.expenses, state.filters)
+
+
   return {
-   expenses: selectExpenses(state.expenses, state.filters),
-   total: getExpensesTotal(state.expenses)
+   expenses: visibleExpenses.length,
+   total: getExpensesTotal(visibleExpenses)
   }
 }
 
