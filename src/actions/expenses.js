43027d935976ce97e68600d0dbb9516131f530db
BYRNE-PC\Warren Byrne <warren.byrne@rezatec.com>
2019-07-05 11:23:35 +0100

    set up tests for adding expenses

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -4,7 +4,7 @@ import database from '../firebase/firebase';
 export const addExpense = (expense) => ({
   type: 'ADD_EXPENSE',
   expense
-})
+});
 
 export const startAddExpense = (expenseData = {}) => {
   return (dispatch) => {
@@ -14,21 +14,24 @@ export const startAddExpense = (expenseData = {}) => {
       amount = 0,
       createdAt = 0
     } = expenseData;
-
     const expense = { description, note, amount, createdAt };
-    database.ref('expenses').push(expense).then(() => {
-      dispatch(addExpense());
+
+    return database.ref('expenses').push(expense).then((ref) => {
+      dispatch(addExpense({
+        id: ref.key,
+        ...expense
+      }));
     });
   };
-}
+};
 
 export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
   id
-})
+});
 
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
   id,
   updates
-})
+});
