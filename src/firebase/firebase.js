0e0273005b5dfc96d49c2b3a3e450d44f0ae8307
Warren Byrne <warren.byrne@rezatec.com>
2019-07-11 17:08:14 +0100

    created basic auth page with firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,10 +7,12 @@ const config = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 }
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, database as default };
+export { firebase, googleAuthProvider, database as default };
