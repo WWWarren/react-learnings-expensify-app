84cd76c8cdf6fcca234bc62080badb802768390b
Warren Byrne <warren.byrne@rezatec.com>
2019-07-12 09:48:21 +0100

    firebase updates

---
+++ src/components/Login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth.js';
+
+class Login extends React.Component {
+  render() {
+    const { startLogin } = this.props;
+    return (
+      <div>
+        <button
+          onClick={startLogin}
+        >
+          Login
+        </button>
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  startLogin: () => dispatch(startLogin())
+})
+
+export default connect(undefined, mapDispatchToProps)(Login);
