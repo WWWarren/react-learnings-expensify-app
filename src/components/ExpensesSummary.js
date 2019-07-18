9bc8435b74149d4485fba6ba6938cb5c6d92e622
Warren Byrne <warren.byrne@rezatec.com>
2019-07-18 14:41:13 +0100

    started styling the app

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 
 import selectExpenses from '../selectors/expenses';
@@ -9,10 +10,13 @@ export const ExpensesSummary = ({ expenses, total }) => {
   const word = expenses === 1 ? 'expense' : 'expenses'
   const totalAmount = numeral(total / 100).format('$0,0.[00]')
   return (
-    <div>
-      <p>
-        Viewing {expenses.length} {word} totalling {totalAmount}
-      </p>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Viewing <span>{expenses}</span> {word} totalling <span>{totalAmount}</span></h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
     </div>
   )
 }
