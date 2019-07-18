96cc7dd9f861fbd734e68d8a7b0d522a20e563e7
Warren Byrne <warren.byrne@rezatec.com>
2019-07-18 17:07:03 +0100

    finished styling the app and added a babel polyfill

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -57,45 +57,48 @@ export default class ExpenseForm extends React.Component {
   }
   render() {
     return (
-      <div>
-        <form onSubmit={this.onSubmit}>
-          <input
-            type="text"
-            placeholder="Description"
-            autoFocus
-            className="text-input"
-            value={this.state.description}
-            onChange={this.onDescriptionChange}
-          />
-          <input
-            type="text"
-            placeholder="Amount"
-            className="text-input"
-            value={this.state.amount}
-            onChange={this.onAmountChange}
-          />
-          <SingleDatePicker
-            date={this.state.createdAt}
-            onDateChange={this.onDateChange}
-            focused={this.state.calendarFocused}
-            onFocusChange={this.onFocusChange}
-            numberOfMonths={1}
-            isOutsideRange={() => false}
-          />
-          <textarea
-            className="textarea"
-            placeholder="Add a note for your expense"
-            value={this.state.note}
-            onChange={this.onNoteChange}
-          >
-          </textarea>
-          {
-            this.state.error &&
-              <p>Please provide a correct description or amount.</p>
-          }
-          <button>Add Expense</button>
-        </form>
-      </div>
+      <form
+        className="form"
+        onSubmit={this.onSubmit}
+      >
+        <input
+          type="text"
+          placeholder="Description"
+          autoFocus
+          className="text-input"
+          value={this.state.description}
+          onChange={this.onDescriptionChange}
+        />
+        <input
+          type="text"
+          placeholder="Amount"
+          className="text-input"
+          value={this.state.amount}
+          onChange={this.onAmountChange}
+        />
+        <SingleDatePicker
+          date={this.state.createdAt}
+          onDateChange={this.onDateChange}
+          focused={this.state.calendarFocused}
+          onFocusChange={this.onFocusChange}
+          numberOfMonths={1}
+          isOutsideRange={() => false}
+        />
+        <textarea
+          className="textarea"
+          placeholder="Add a note for your expense"
+          value={this.state.note}
+          onChange={this.onNoteChange}
+        >
+        </textarea>
+        {
+          this.state.error &&
+            <p className="form__error">Please provide a correct description or amount.</p>
+        }
+        <div>
+          <button className="button">Save Expense</button>
+        </div>
+      </form>
     )
   }
 }
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -6,16 +6,25 @@ import selectExpenses from '../selectors/expenses';
 import ExpenseListItem from './ExpenseListItem';
 
 export const ExpenseList = (props) => (
-  <div>
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
+    <div className="list-body">
     {
       props.expenses.length === 0 ? (
-        <p>No expenses</p>
+        <div className="list-item__message">
+          <span>No expenses</span>
+        </div>
       ) : (
         props.expenses.map((p, i) => (
           <ExpenseListItem {...p} key={i} />
         ))
       )
     }
+    </div>
   </div>
 );
 
