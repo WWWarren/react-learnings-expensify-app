8aa1c0c287f91a82b9f5ea809529485d6e696033
Warren Byrne <warren.byrne@rezatec.com>
2019-07-18 11:17:33 +0100

    set up public routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { startLogout } from '../actions/auth';
 export const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="isActive" exact={true}>Home</NavLink>
+    <NavLink to="/dashboard" activeClassName="isActive">Home</NavLink>
     <NavLink to="/create" activeClassName="isActive">Add</NavLink>
     <button onClick={startLogout}>Logout</button>
   </header>
